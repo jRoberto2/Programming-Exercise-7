44b55f389e60d8b6f98d6de280c19a0b80e57db8
Julio A. Roberto <jRoberto2@mail.valenciacollege.edu>
2020-07-11 23:45:33 -0400

    Finished addItem function

--- Script.js
+++ Script.js
@@ -1,24 +1,12 @@
 //Progamming Exercise 7
 
-var list = document.querySelector("ol");
-var i;
-
-
 //When you submit the form, a new to-do should pop up in your to-do list
 function addItem() {
-    item = document.createElement("li");
-    item.text = document.createTextNode("");
-    item.appendChild(item.text);
+    var list = document.getElementById("list");
+    var item = document.createElement("li");
+    var input = document.getElementById("input");
+    var value = input.value;
+    var itemText = document.createTextNode(value);
+    item.appendChild(itemText);
     list.appendChild(item);
 }
-
-//Submitting the form again should add another to do below the previous to-do
-
-//Use an array to keep track of the todos (Meaning every time the form is submitted, you push the new value onto the array)
-todos = [
-
-]
-
-//Build a template from the array using the map method learned about in programming exercise 6.
-
-//Display the template on the screen
